f4bc3272aeb49f283a23136c86f8c2e630705f0f
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-05-19 21:15:19 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import "./App.css";
-import Add from './components/Add/Add';
-import Sub from './components/Sub/Sub';
-
+import Add from "./components/Add/Add";
+import Sub from "./components/Sub/Sub";
+import { useState } from "react";
 function App() {
-  
+  const [inputValue, setInputValue] = useState(0);
+  const [addValue, setaddValue] = useState(0);
+  const printValue = () => {
+    console.log("inputeValue printValue", inputValue);
+    return inputValue;
+  };
   return (
     <div>
       <h1>Welcome React First App </h1>
- 
-     <Add a='2' b='2' ></Add>
-     <Sub a='2' b='2'></Sub>
+      <div>
+        <input
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            console.log("inputValue", inputValue);
+          }}
+          type="number"
+          placeholder="Enter Number"
+        />
+      </div>
+      <div>
+        <input
+          value={addValue}
+          onChange={(e) => {
+            setaddValue(e.target.value);
+            console.log("addValue", addValue);
+          }}
+          type="number"
+          placeholder="Enter Number"
+        />
+      </div>
+
+      <p>Add Number{inputValue}</p>
+<p>Add value{addValue}</p>
+      <button onClick={printValue}>Print </button>
+      <Add a={inputValue} b={addValue}></Add>
+      <Sub a={inputValue} b={addValue}></Sub>
     </div>
   );
 }
